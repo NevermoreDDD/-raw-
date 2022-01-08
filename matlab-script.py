62e1bebdeaf239ea9039fb49ce81700106926a32
Xingchen <719311857@qq.com>
2022-01-08 15:38:04 +0800

    使用递归方法解决了源文件夹中嵌套文件夹的问题

--- matlab-script.py
+++ matlab-script.py
@@ -7,17 +7,8 @@ import pandas as pd
 
 # PATH = '//192.168.2.58/weld_data_line/temppp/A-04/A-10'
 # files = glob.glob(PATH+'/*'+'.raw')
-PATH = ''
 savepath= input("请输入储存数据的目录（绝对路径）：").replace("\\",'/')
 workdir = input('输入源文件的绝对路径: ').replace("\\",'/')
-parameters = {
-    'filepath': PATH,
-    'pathF': '眩光',
-    'savepath': """C:/桌面/000/gamma变换 + 保存图片/gamma变换 + 保存图片""",
-    'width': 1056,
-    'height': 1920,
-    'bit': 'uint8',
-}
 
 # engine = matlab.engine.start_matlab()
 # file_list = os.listdir(PATH)
@@ -53,27 +44,16 @@ class ResolutionException(Exception):
     pass
 
 class SavePng():
-    def __init__(self, filepath = PATH, pathF = None, savepath=savepath, width=None, height=None, bit=None):
+    def __init__(self, pathF = None, savepath=savepath, width=None, height=None, bit=None):
         """
         传入matlab的参数
         """
-        self.filepath = self._getpath()
         self.pathF = pathF
         self.savepath = savepath
         self.width = width
         self.height = height
         self.bit = bit
         self.engine = matlab.engine.start_matlab()
-    def _getpath(self):
-        """
-        获得源图片路径
-        """
-        while True:
-            workdir = input('输入绝对路径: ').replace("\\",'/')
-            if workdir == '':
-                print("没有那个文件")
-            else:
-                return workdir
     def _select_resolution(self,files):
         """
         根据图片大小选择合适的分辨率和比特
@@ -107,45 +87,43 @@ class SavePng():
             else:
                 # 如果文件不满足预设的条件，抛出异常
                 raise ResolutionException("没有定义这样的分辨率数据，请检查代码")
+        return
     def set_parameter(self,filepath):
         """
         设定需要传入matlab的参数并且调用matlab脚本
         """
         # 获取源目录下的所有文件
         file_list = os.listdir(filepath)
+        # print(file_list)
         # 获取存储路径下的所有文件
         save_dir = os.listdir(self.savepath)
         exist_save = []
         for save in save_dir:
-            save = self.savepath + '/' + save
-            if os.path.isdir(save):
+            save_path = self.savepath + '/' + save
+            if os.path.isdir(save_path):
                 exist_save.append(save)
+                # print(exist_save)
         exist_save = [x for x in re.findall(r"[^ ]*(?=ScaleRaw|Scale512)", " ".join(exist_save)) if x != '']
         root_file = []
         for item in file_list:
-            path = self.filepath + '/' + item
-            i = item
-            while i in exist_save:
-                i = input("该文件已存在于savepath中，请写一个新名字（只能用英文或者数字）:")
+            path = filepath + '/' + item
             if os.path.isdir(path):
-                self._select_resolution(path)
-                self.pathF = i
-                self.connect_matlab(path)
+                self.set_parameter(path)
             else:
                 root_file.append(item)
-        print(root_file)
+        # print(root_file)
         if len(root_file) != 0:
-            i = re.split(r'/',self.filepath)[-1]
+            i = re.split(r'/',filepath)[-1]
             print(i)
             while i in exist_save:
                 i = input("该文件已存在于savepath中，请写一个新名字（只能用英文或者数字）:")
-            self._select_resolution(self.filepath)
+            self._select_resolution(filepath)
             self.pathF = i
-            self.connect_matlab(self.filepath)
+            self.connect_matlab(filepath)
         return
 
     def connect_matlab(self,dir):
-        print('即将读取的目录： {},这里有{}张图片，请确认(y/n)'.format(dir,len(os.listdir(dir))))
+        print('即将读取的目录： {},这里有{}张图片，目前设定最多只会读取300张图片，如需要请更改load_raw_File.m'.format(dir,len(os.listdir(dir))))
         # check = input()
         # if check.lower() == 'y':
         with open("loadFile.txt",'w',encoding='utf-8') as f:
@@ -157,6 +135,7 @@ class SavePng():
             f.write(self.bit+'\n')
         self.engine.savePng(nargout=0)
         print("执行完毕")
+        return
         # else:
         #     return
     def start(self):
@@ -164,6 +143,6 @@ class SavePng():
 if __name__ == '__main__':
     while True:
         a = SavePng()
-        a.set_parameter()
+        a.set_parameter(workdir)
         del a
 
