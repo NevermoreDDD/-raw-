b43f4b3f8ae38aece3530e9f43bd96dd088e3261
Xingchen <719311857@qq.com>
2022-01-12 13:54:18 +0800

    移动PNG文件位置并且以它们的MD5值重命名

--- matlab-script.py
+++ matlab-script.py
@@ -3,12 +3,11 @@ import matlab.engine
 import os
 import re
 import glob
-import pandas as pd
+from collections import defaultdict
 
-# PATH = '//192.168.2.58/weld_data_line/temppp/A-04/A-10'
+# PATH = '//192/home/fullv/workspace/projects/NeuralNetWork/MMSegmentation_Tutorial.ipynb.168.2.58/weld_data_line/temppp/A-04/A-10'
 # files = glob.glob(PATH+'/*'+'.raw')
-savepath= input("请输入储存数据的目录（绝对路径）：").replace("\\",'/')
-workdir = input('输入源文件的绝对路径: ').replace("\\",'/')
+
 
 # engine = matlab.engine.start_matlab()
 # file_list = os.listdir(PATH)
@@ -44,7 +43,7 @@ class ResolutionException(Exception):
     pass
 
 class SavePng():
-    def __init__(self, pathF = None, savepath=savepath, width=None, height=None, bit=None):
+    def __init__(self, pathF=None, savepath=None, width=None, height=None, bit=None):
         """
         传入matlab的参数
         """
@@ -60,10 +59,11 @@ class SavePng():
         """
         print(f"正在读取{files}...")
         # 获得文件夹中所有后缀为.raw的文件
-        files = glob.glob(files + '/*' + '.raw')
-        if not files:
+        raw_files = glob.glob(files + '/*' + '.raw')
+
+        if not raw_files:
             return
-        for file in files:
+        for file in raw_files:
             if round(os.stat(file).st_size / 1024) in [3038, 3037]:
                 self.width = 1440
                 self.height = 1080
@@ -86,9 +86,9 @@ class SavePng():
                 break
             else:
                 # 如果文件不满足预设的条件，抛出异常
-                raise ResolutionException("没有定义这样的分辨率数据，请检查代码")
+                skip_files[f'{files}'].append(os.stat(file).st_size / 1024)
         return
-    def set_parameter(self,filepath):
+    def set_parameter(self, filepath, handle_duplicate=None):
         """
         设定需要传入matlab的参数并且调用matlab脚本
         """
@@ -108,22 +108,24 @@ class SavePng():
         for item in file_list:
             path = filepath + '/' + item
             if os.path.isdir(path):
-                self.set_parameter(path)
-            else:
+                self.set_parameter(path,handle_duplicate)
+            elif len(re.findall(r"\.raw$", item)) != 0:
                 root_file.append(item)
         # print(root_file)
         if len(root_file) != 0:
             i = re.split(r'/',filepath)[-1]
             print("目标文件夹前缀：", i)
+            # 会自动在结尾加上一个数字，这个数字不一定是连续的
             while i in exist_save:
-                i = input("该文件已存在于savepath中，请写一个新名字（只能用英文或者数字）:")
+                i += str(handle_duplicate)
+                handle_duplicate += 1
             self._select_resolution(filepath)
             self.pathF = i
             self.connect_matlab(filepath)
         return
 
     def connect_matlab(self,dir):
-        print('即将读取的目录： {},这里有{}张图片，目前设定最多只会读取300张图片，如需要请更改load_raw_File.m'.format(dir,len(os.listdir(dir))))
+        print('即将读取的目录： {},这里有{}张图片，目前设定最多只会读取300张图片，如需要请更改load_raw_File.m'.format(dir,len(glob.glob(dir + '/*' + '.raw'))))
         # check = input()
         # if check.lower() == 'y':
         with open("loadFile.txt",'w',encoding='utf-8') as f:
@@ -141,8 +143,16 @@ class SavePng():
     def start(self):
         pass
 if __name__ == '__main__':
+    savepath = input("请输入储存数据的目录（绝对路径）：").replace("\\", '/')
+    # 全局变量，用来应对重名文件过多的问题
+    handle_duplicate = 0
+    skip_files = defaultdict(list)
     while True:
-        a = SavePng()
-        a.set_parameter(workdir)
+        workdir = input('输入源文件的绝对路径(直接按回车可以停止脚本): ').replace("\\", '/')
+        if workdir == '':
+            break
+        a = SavePng(savepath=savepath)
+        a.set_parameter(workdir,handle_duplicate)
         del a
+
 
