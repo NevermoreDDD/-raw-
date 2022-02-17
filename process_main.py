28c2a3fe2ad8d78f21df105f3d99440fe12b3b37
Xingchen <719311857@qq.com>
2022-02-17 15:59:58 +0800

    添加了注释

--- process_main.py
+++ process_main.py
@@ -1,3 +1,14 @@
+"""
+说明：
+本脚本用来自动处理raw格式的文件，使用教程已经写在readme中
+包含的类：
+Logger: 日志类，定义了日志的输出格式和内容。
+方法：
+select_resolution: 根据图像的大小，自动分配人工预设置的分辨率
+set_parameter: 脚本的主函数，包括递归查找存放raw文件的所有子目录，创建存储路径，一旦找到最底层的文件夹，就调用图片处理方法
+process_raw: 图片处理方法，可以读取目录下的raw文件，通过调用process_raw模块下的方法，将raw文件转换成png文件，并且可以赋予它们连续的ID
+"""
+
 import cv2
 import os
 import re
@@ -6,26 +17,32 @@ import logging
 import logging.handlers
 from process_raw import RawFile
 import hashlib
-import random
 from tqdm import tqdm
 
 
 class Logger:
+    """
+    日志类，实例化的时候不需要传入任何参数，当然也可以参考官方文档设置输出日志的等级，但是不建议修改。
+    """
     def __init__(self, flevel=logging.INFO, clevel=logging.INFO):
+        # 获取日志文件
         self.logger = logging.getLogger('auto-process.log')
         self.logger.setLevel(logging.INFO)
+        # 设置日志输出的格式
         fmt = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s ', '%Y-%m-%d %H:%M:%S')
         ch = logging.StreamHandler()
         ch.setFormatter(fmt)
         ch.setLevel(clevel)
+        # 每小时分割一次日志。
         fh = logging.handlers.TimedRotatingFileHandler('auto_process.log', when='H', interval=1)
         # fh.namer = lambda x: x.split('.')[1]
+        # 分割后的日志后缀
         fh.suffix = "%Y-%m-%d_%H.log"
         fh.setFormatter(fmt)
         fh.setLevel(flevel)
         self.logger.addHandler(fh)
         self.logger.addHandler(ch)
-
+    # 以下函数是日志对应的方法，不需要修改
     def debug(self, message):
         self.logger.debug(message)
 
@@ -42,44 +59,20 @@ class Logger:
         self.logger.critical(message)
 
 
-def _get_last_line(filename):
-    """
-    get last line of a file
-    :param filename: file name
-    :return: last line or None for empty file
-    """
-    try:
-        filesize = os.path.getsize(filename)
-        if filesize == 0:
-            return None
-        else:
-            with open(filename, 'rb') as fp:  # to use seek from end, must use mode 'rb'
-                offset = -8  # initialize offset
-                while -offset < filesize:  # offset cannot exceed file size
-                    fp.seek(offset, 2)  # read # offset chars from eof(represent by number '2')
-                    lines = fp.readlines()  # read from fp to eof
-                    if len(lines) >= 2:  # if contains at least 2 lines
-                        return lines[-1]  # then last line is totally included
-                    else:
-                        offset *= 2  # enlarge offset
-                fp.seek(0)
-                lines = fp.readlines()
-                return lines[-1]
-    except FileNotFoundError:
-        print(filename + ' not found!')
-        return None
-
-
 def select_resolution(files, logger):
     """
-    根据图片大小选择合适的分辨率和比特
+    根据图片大小选择合适的分辨率和比特。
+    注意：方法默认一个目录下的所有图片的参数都一致。如果实际出现不一致的情况，在后续处理（其他函数中）将会记录在日志里。
+    files: 目录（这个目录下不存在子目录）
+    logger: Logger类的实例化对象
     """
     print(f"正在读取{files}...")
     # 获得文件夹中所有后缀为.raw的文件
     raw_files = glob.glob(files + '/*' + '.raw')
-
+    # 如果该目录下不存在raw文件，就返回
     if not raw_files:
         return
+    # 遍历文件， 如果找到对应的分辨率目标，就直接返回结果
     for file in raw_files:
         if round(os.stat(file).st_size / 1024) in [3038, 3037]:
             width = 1440
@@ -116,7 +109,7 @@ def select_resolution(files, logger):
             height = 1072
             bit = 'uint16'
             return width, height, bit
-        elif round(os.stat(file).st_size / 1024) in [1518, 1519,1520]:
+        elif round(os.stat(file).st_size / 1024) in [1518, 1519, 1520]:
             width = 720
             height = 1080
             bit = 'uint16'
@@ -127,40 +120,50 @@ def select_resolution(files, logger):
             bit = 'uint8'
             return width, height, bit
         else:
-            # 如果文件不满足预设的条件，抛出异常
+            # 如果文件不满足预设的条件，就把这个文件记录在日志中，继续检查下一个文件
             logger.error(f"{file} size of {round(os.stat(file).st_size / 1024)} KB. Please Check Manually")
             continue
+    # 如果遍历结束仍未返回，为了避免程序中断，会将这个文件夹记录在日志中。并且返回(0, 0, 0)
     logger.error(f"{files} has totally new resolution")
     return 0, 0, 0
 
 
 def set_parameter(filepath, logger, savepath: str, handle_duplicate=None):
     """
-    设定需要传入matlab的参数并且调用matlab脚本
-    :param handle_duplicate:
-    :param filepath:
-    :type savepath: str
+    主函数
+    1. 从日志中读取内容，记录已经处理过的目录。
+    2. 查看存储路径下是否有同名文件，如果有的话会在文件结尾加一个数字。
+    3. 递归搜索文件的所有子目录
+    4. 查找到最底层目录后，调用select_resolution函数，获取该目录下raw文件的WIDTH HEIGHT BIT.
+    5. 获取需要的信息之后调用process_raw函数，转存raw图为png图片
+    :param filepath: str，目前正在读取的目录
+    :param logger: Logger对象
+    :param savepath: str，存储路径
+    ## 目前会生成多个exist_save和多个done_list，可以把145-172行单独拆出来一个函数，并作为参数传递进来。减少运算量和内存占用
     """
-    process_id = os.getpid()
-
     # 获取源目录下的所有文件
     file_list = os.listdir(filepath)
     # print(file_list)
     # 获取存储路径下的所有文件的名字
     save_dir = os.listdir(savepath)
     exist_save = []
+    # 遍历文件名字
     for save in save_dir:
         save_path = savepath + '/' + save
+        # 判断获取到的文件是否是一个目录，如果是的话就加到exist_save里面
         if os.path.isdir(save_path):
             exist_save.append(save)
             # print(exist_save)
+    # 对exist_save做二次筛选，以ScaleRaw或Scale512作为后缀来获取文件夹的实际名字。
     exist_save = [x for x in re.findall(r"[^ ]*(?=ScaleRaw|Scale512)", " ".join(exist_save)) if x != '']
     # 这里要读日志，然后获取已经处理过的文件夹，如果存在在日志中，就跳过
     log_files = glob.glob(os.getcwd() + "/*" + '.log')
     try:
+        # 对文件夹按照日期排序，之前遇到bug了，所以用try来规避掉indexerror。
         log_files.sort(key=lambda x: re.findall(r'(?<=auto_process).*(?=.log)', x)[0])
     except IndexError:
         pass
+    # 如果因为IndexError导致没办法排序，就不排了，继续执行下面的语句。
     finally:
         done_list = []
         for log in log_files:
@@ -169,15 +172,19 @@ def set_parameter(filepath, logger, savepath: str, handle_duplicate=None):
                     logs = l.readlines()
                     data = ' '.join(logs)
                     # print(re.findall(r'(?<=Work done: ).*(?= finished)', data))
+                    # 这里的格式是自定义的，会获取Work done: finished中间的字符串
                     done_list += re.findall(r'(?<=Work done: ).*(?= finished)', data)
                 except IndexError:
                     done_list = []
+        # 定义一个root_file，用来判断是不是已经到了最底层。
         root_file = []
         for item in file_list:
             path = filepath + '/' + item
             if os.path.isdir(path):
+                # 如果已经存在在done_list里面，说明这个文件夹处理过了，跳过
                 if path in done_list:
                     continue
+                # 如果不在，就再调用一次set_parameter
                 set_parameter(path, logger, savepath, handle_duplicate)
             elif len(re.findall(r"\.raw$", item)) != 0:
                 root_file.append(item)
@@ -237,7 +244,7 @@ def process_raw(directory, logger, save_path, bit, width, height):
                 md.update(data)
                 file_name = md.hexdigest()
             os.remove(os.path.join(save_path, 'temp' + '.png'))
-            with open(os.path.join(save_path,"{:0>7d}_".format(true_id) + file_name + '.png'), 'wb') as f:
+            with open(os.path.join(save_path, "{:0>7d}_".format(true_id) + file_name + '.png'), 'wb') as f:
                 f.write(data)
             true_id += 1
     # self.engine.savePng(nargout=0)
